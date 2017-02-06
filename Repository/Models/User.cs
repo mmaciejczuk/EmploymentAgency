3dcf91a7170a8491a324b2de5de55d1b6f4d762e
Mariusz <mariusz.maciejczuk@softwarehut.com>
2017-02-06 15:50:49 +0100

    last commit

--- Repository/Models/User.cs
+++ Repository/Models/User.cs
@@ -24,10 +24,21 @@ namespace Repository.Models
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Required]
         public DateTime? DateOfBirth { get; set; }
-
+        [Display(Name = "Adres email:")]
+        [Required]
+        [MaxLength(160)]
         public string Email { get; set; }
+        [Display(Name = "Ulica i numer:")]
+        [Required]
+        [MaxLength(100)]
         public string Street { get; set; }
+        [Display(Name = "Kod pocztowy:")]
+        [Required]
+        [MaxLength(10)]
         public string Code { get; set; }
+        [Display(Name = "Miasto:")]
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; }
         public bool IsActive { get; set; }
         [Display(Name = "Data dodania:")]
@@ -36,6 +47,7 @@ namespace Repository.Models
         [Required]
         public DateTime AddedDate { get; set; }
 
+        [Required]
         public Role Role { get; set; }
     }
 }
